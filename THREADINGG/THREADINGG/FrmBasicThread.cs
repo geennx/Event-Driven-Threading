db39405f23b01d03ceff3b38d7c6fca162cfce39
geennx <109021591+geennx@users.noreply.github.com>
2022-10-28 12:55:15 +0800

    Add files via upload

---
+++ THREADINGG/THREADINGG/FrmBasicThread.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+using System;
+
+namespace THREADINGG
+{
+    public partial class FrmBasicThread : Form
+    {
+        ThreadStart delThread = new ThreadStart(MyThreadClass.Thread1);
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Console.WriteLine("-Before Starting a Thread-");
+            Thread ThreadA = new Thread(delThread);
+            Thread ThreadB = new Thread(delThread);
+
+            ThreadA.Name = "Thread A process";
+            ThreadB.Name = "Thread B process";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            if (!ThreadA.IsAlive && !ThreadB.IsAlive)
+            {
+                label1.Text = "-End of Thread-";
+                Console.WriteLine("-End of thread");
+                
+            }
+        }
+    }
+}
---
+++ THREADINGG/THREADINGG/MyThreadClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace THREADINGG
+{
+    class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            
+            for (int i = 0; i<=5; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: {0} = {1} ",thread.Name,i);
+                Thread.Sleep(1500);
+                
+
+                if (i == 5)
+                {
+                    //thread.Abort();
+                }
+
+            }
+        }
+    }
+}
